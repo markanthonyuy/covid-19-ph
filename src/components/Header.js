7905267f0f1d73b37136c6dc1c2d0a2b0dbbcb9b
markanthonyuy <macmac.uy@gmail.com>
2020-04-13 10:28:03 +0800

    fix header title css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import bacteria from '../img/bacteria.svg'
 
 const Header = () => {
   return (
-    <header className="App-header p-10">
+    <header className="App-header p-6">
       <img src={bacteria} alt="" width="50" className="logo" />
-      <h1 className="w-full text-5xl leading-tight text-center">
+      <h1 className="w-full text-4xl mt-2 leading-none text-center font-hairline">
         COVID 19 PH Tracker
       </h1>
     </header>
