d1663f744cec8ac58963852bc275d6e917ed5150
markanthonyuy <macmac.uy@gmail.com>
2020-04-16 15:02:14 +0800

    add hover effect per card

--- src/components/Box.js
+++ src/components/Box.js
@@ -6,7 +6,7 @@ const Box = (props) => {
   return (
     <Fade delay={props.delay}>
       <div className="md:float-left md:w-1/3 border-8 border-transparent">
-        <div className="box shadow-md bg-white rounded-md box flex flex-col justify-center md:justify-start p-3 md:p-5 align-center">
+        <div className="box shadow-md hover:shadow-lg bg-white rounded-md box flex flex-col justify-center md:justify-start p-3 md:p-5 align-center">
           <p className="text-md">{props.title}</p>
           <span className={`${props.classNameCount} font-medium`}>
             <Count end={props.count} onEnd={props.onEnd} />
