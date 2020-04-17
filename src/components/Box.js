ebe70a4d3a4a576bcf83400756329ba98e6b83e8
markanthonyuy <macmac.uy@gmail.com>
2020-04-17 11:32:49 +0800

    add alt, fix percentage not showing

--- src/components/Box.js
+++ src/components/Box.js
@@ -9,12 +9,7 @@ const Box = (props) => {
         <div className="box shadow-md hover:shadow-lg bg-white rounded-md box flex flex-col justify-center md:justify-start p-3 md:p-5 align-center">
           <p className="text-md">{props.title}</p>
           <span className={`${props.classNameCount} font-medium`}>
-            <Count
-              end={props.count}
-              onEnd={props.onEnd}
-              delay={props.delay}
-              duration={3}
-            />
+            <Count end={props.count} onEnd={props.onEnd} delay={1} />
           </span>
           {props.hasPercent && (
             <span className={props.percentVisibility}>
