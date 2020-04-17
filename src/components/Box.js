a338059100f81927f6bd0ac96cc51412f7b23e08
markanthonyuy <macmac.uy@gmail.com>
2020-04-17 09:04:38 +0800

    disable selection

--- src/components/Box.js
+++ src/components/Box.js
@@ -5,7 +5,7 @@ import Fade from 'react-reveal/Fade'
 const Box = (props) => {
   return (
     <Fade delay={props.delay}>
-      <div className="md:float-left md:w-1/3 border-8 border-transparent">
+      <div className="md:float-left md:w-1/3 border-8 border-transparent select-none">
         <div className="box shadow-md hover:shadow-lg bg-white rounded-md box flex flex-col justify-center md:justify-start p-3 md:p-5 align-center">
           <p className="text-md">{props.title}</p>
           <span className={`${props.classNameCount} font-medium`}>
