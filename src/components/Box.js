e829c70819fab3f92e4fedc3dfc1db64ecedd379
markanthonyuy <macmac.uy@gmail.com>
2020-04-17 09:58:08 +0800

    add delay and duration to countup

--- src/components/Box.js
+++ src/components/Box.js
@@ -9,7 +9,12 @@ const Box = (props) => {
         <div className="box shadow-md hover:shadow-lg bg-white rounded-md box flex flex-col justify-center md:justify-start p-3 md:p-5 align-center">
           <p className="text-md">{props.title}</p>
           <span className={`${props.classNameCount} font-medium`}>
-            <Count end={props.count} onEnd={props.onEnd} />
+            <Count
+              end={props.count}
+              onEnd={props.onEnd}
+              delay={props.delay}
+              duration={3}
+            />
           </span>
           {props.hasPercent && (
             <span className={props.percentVisibility}>
